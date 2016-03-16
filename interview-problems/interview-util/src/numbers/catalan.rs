68da3e2a7a938c1d229e0fa9ed51df773f35a0dd
YetAnotherMinion <yetanotherminion@gmail.com>
2016-03-16 18:04:01 -0400

    implemented catalan numbers

--- interview-problems/interview-util/src/numbers/catalan.rs
+++ interview-problems/interview-util/src/numbers/catalan.rs
@@ -8,3 +8,16 @@ pub fn catalan<T>(n: T) -> T
     binomial(two * n, n) / ( n + T::one())
 }
 
+#[cfg(test)]
+mod test {
+    #[test]
+    fn spot_check_catalan() {
+        assert_eq!(1, super::catalan(1));
+        assert_eq!(2, super::catalan(2));
+        assert_eq!(5, super::catalan(3));
+        assert_eq!(14, super::catalan(4));
+        assert_eq!(42, super::catalan(5));
+    }
+
+}
+
--- interview-problems/interview-util/src/numeric/field.rs
+++ interview-problems/interview-util/src/numeric/field.rs
@@ -3,5 +3,13 @@ use numeric::{Zero, One};
 
 pub trait Field : Sized + Zero + One + Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> + PartialOrd + Eq{}
 
-//impl Field for T where T: Sized + Zero + One + Add + Sub + Mul + Div {}
-
+impl Field for u8 {}
+impl Field for u16 {}
+impl Field for u32 {}
+impl Field for u64 {}
+impl Field for usize {}
+impl Field for i8 {}
+impl Field for i16 {}
+impl Field for i32 {}
+impl Field for i64 {}
+impl Field for isize {}
