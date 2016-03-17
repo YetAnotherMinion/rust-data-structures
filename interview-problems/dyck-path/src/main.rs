6956d90a6ccc4f8ce82b399940653417af48c408
YetAnotherMinion <yetanotherminion@gmail.com>
2016-03-17 18:41:16 -0400

    solved dyack path problem using catalan numbers

--- interview-problems/dyck-path/src/main.rs
+++ interview-problems/dyck-path/src/main.rs
@@ -1,7 +1,14 @@
 extern crate yam_interview_util;
 
+use std::env;
 use yam_interview_util::numbers::catalan;
 
 fn main() {
-    println!("Hello, world! {:?}", catalan(5));
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 3 {
+        return;
+    }
+    let n: u64 = args[1].parse().ok().expect("Wanted a number");
+    let k: u64 = args[2].parse().ok().expect("Wanted a number");
+    println!("Number of Dyack Paths is {}", catalan(n));
 }
